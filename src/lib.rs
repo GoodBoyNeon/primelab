e641b06b35295d00353bf0688a7e5aba65f63b3c
Sushant Ray <goodboyneon03@gmail.com>
2026-03-29 21:35:25 +0545

    feat: add fermat to CLI

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,8 @@ use napi_derive::napi;
 pub fn miller_rabin(n: u32, iterations: u32) -> f64 {
   primality::miller_rabin::test(n, iterations)
 }
+
+#[napi]
 pub fn fermat(n: u32, iterations: u32) -> f64 {
   primality::fermat::test(n, iterations)
 }
--- ui/index.ts
+++ ui/index.ts
@@ -1,11 +1,28 @@
-import { millerRabin } from '../index.js'
+import { millerRabin, fermat } from '../index.js'
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
 const cli = yargs(hideBin(process.argv))
 
-const testAlgorithms = ['miller-rabin'] as const
+const testAlgorithms = ['miller-rabin', 'fermat'] as const
 type TestAlgorithm = (typeof testAlgorithms)[number]
+
+function handleResult(number: number, probability: number, raw: boolean) {
+  const MAX_SAFE_INPUT = 3_037_000_499
+  if (number > MAX_SAFE_INPUT) {
+    console.log(`Error: ${number} exceeds the maximum supported value of ${MAX_SAFE_INPUT}`)
+    process.exit(1)
+  }
+  if (probability === -1) {
+    console.log(`${number} is neither prime nor composite.`)
+  } else if (probability === 0) {
+    console.log(`${number} is composite.`)
+  } else {
+    console.log(
+      `${number} is ${probability !== 1 ? 'probably ' : ''}prime\n:: Confidence : ${raw ? probability * 100 : (probability * 100).toFixed(4)}%`,
+    )
+  }
+}
 
 cli
   .command(
@@ -39,24 +56,18 @@ cli
     (argv) => {
       const { number, algorithm, iterations, raw } = argv
 
+      console.log(`Testing ${number} with ${algorithm} for ${iterations} iteration${iterations === 1 ? '' : 's'}...`)
+
       switch (algorithm) {
         case 'miller-rabin': {
-          const MAX_SAFE_INPUT = 3_037_000_499
           const probability = millerRabin(number, iterations)
-
-          if (number > MAX_SAFE_INPUT) {
-            console.log(`Error: ${number} exceeds the maximum supported value of ${MAX_SAFE_INPUT}`)
-            process.exit(1)
-          }
-          if (probability === -1) {
-            console.log(`${number} is neither prime nor composite.`)
-          } else if (probability === 0) {
-            console.log(`${number} is composite.`)
-          } else {
-            console.log(
-              `${number} is ${probability !== 1 ? 'probably ' : ''}prime\n:: Confidence : ${raw ? probability * 100 : (probability * 100).toFixed(4)}%`,
-            )
-          }
+          handleResult(number, probability, raw ?? false)
+          break
+        }
+        case 'fermat': {
+          const probability = fermat(number, iterations)
+          handleResult(number, probability, raw ?? false)
+          break
         }
       }
     },
